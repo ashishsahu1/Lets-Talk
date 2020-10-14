79663521a8a0f565746b355d8f6a8eedaf356fbe
ashishsahu1 <ashishsahu1999@hotmail.com>
2020-10-15 02:34:40 +0530

    Initial Flutter Setup
    removed debug banner
    added screens(views)

---
+++ lets_talk/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:lets_talk/views/signin.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+    
+        primarySwatch: Colors.blue,
+        
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignIn()
+    );
+  }
+}
